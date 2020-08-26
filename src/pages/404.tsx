c12abb14cc67af1d12900ee31e387e7cf9b7671d
misteroh <madeline@done-digital.com>
2020-08-26 15:57:24 -0700

    updates

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,13 +2,17 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { ThemeProvider } from "styled-components"
+import { theme } from "../styled/theme"
 
 const NotFoundPage = () => (
+  <ThemeProvider theme={theme}>
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
+ </ThemeProvider>
 )
 
 export default NotFoundPage
