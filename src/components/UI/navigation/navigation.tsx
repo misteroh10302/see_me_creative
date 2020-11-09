81e8153e8311990585b90f8b81c0c2530db45895
misteroh <madeline@done-digital.com>
2020-11-09 14:38:11 -0800

    fix/add load styles to nav

--- src/components/UI/navigation/navigation.tsx
+++ src/components/UI/navigation/navigation.tsx
@@ -2,9 +2,17 @@ import { Link } from "gatsby"
 import * as React from "react"
 
 import { NavigationWrapper } from "./navigationWrapper"
+const navStyles = {
+  position: "fixed",
+  top: "0px",
+  textAlign: "center",
+  width: "100%",
+  padding: "2rem",
+  zIndex: 1000,
+}
 
 const Navigation = (props: NavigationProps) => (
-  <NavigationWrapper>
+  <NavigationWrapper style={navStyles}>
     <h1 style={{ margin: 0 }}>
       <Link to="/">
         <img
