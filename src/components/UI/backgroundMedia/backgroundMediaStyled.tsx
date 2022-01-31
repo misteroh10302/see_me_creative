66b9207dcb021b9a4e4f94932d96587de983e50d
misteroh <madeline.omoore@generalassemb.ly>
2022-01-30 21:35:35 -0800

    fx/hide play btn

--- src/components/UI/backgroundMedia/backgroundMediaStyled.tsx
+++ src/components/UI/backgroundMedia/backgroundMediaStyled.tsx
@@ -12,6 +12,49 @@ export const BackgroundMediaWrapper = styled.section`
   video::-webkit-media-controls {
       opacity: 0 !important;
   }
+      /* This used to work for the parent element of button divs */
+    /* But it does not work with newer browsers, the below doesn't hide the play button parent div */
+
+    *::-webkit-media-controls-panel {
+      display: none!important;
+      -webkit-appearance: none;
+    }
+    *::-webkit-media-controls-panel {
+  display: none!important;
+ -webkit-appearance: none;
+}
+
+/* Old shadow dom for play button */
+
+*::--webkit-media-controls-play-button {
+  display: none!important;
+  -webkit-appearance: none;
+}
+
+/* New shadow dom for play button */
+
+/* This one works */
+
+*::-webkit-media-controls-start-playback-button {
+  display: none!important;
+  -webkit-appearance: none;
+}
+
+    /* Old shadow dom for play button */
+
+    *::-webkit-media-controls-play-button {
+      display: none!important;
+      -webkit-appearance: none;
+    }
+
+    /* New shadow dom for play button */
+
+    /* This one works! */
+
+    *::-webkit-media-controls-start-playback-button {
+      display: none!important;
+      -webkit-appearance: none;
+    }
   .desktop-video-background {
     display: none;
   }
