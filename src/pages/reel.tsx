5a5af2887ea05d8b6cb994e9c44ca1d22b1a3801
misteroh <madeline.omoore@generalassemb.ly>
2023-02-24 08:41:11 -0800

    feat: add footer to reel page

--- src/pages/reel.tsx
+++ src/pages/reel.tsx
@@ -6,6 +6,7 @@ import { theme } from "../styled/theme"
 import { ReelWrapper } from "../styled/layoutStyles"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Footer from "../components/UI/footer/footer"
 
 const reelQuery = graphql`
   query reelQuery {
@@ -14,6 +15,58 @@ const reelQuery = graphql`
       vimeoId
       vimeoIdMobile
     }
+    allContentfulOurWork {
+      nodes {
+        footer {
+          content {
+            json
+          }
+          buttons {
+            link
+            buttonText
+          }
+        }
+        footerBackground {
+          file {
+            contentType
+            url
+          }
+          fluid {
+            ...GatsbyContentfulFluid_withWebp_noBase64
+          }
+        }
+      }
+    }
+    allContentfulBlackPageMeshGrids {
+      nodes {
+        meshGridTop {
+          file {
+            contentType
+            url
+          }
+        }
+        meshTopGridMobile {
+          file {
+            url
+            fileName
+            contentType
+          }
+        }
+        meshGridBottomMobile {
+          file {
+            fileName
+            url
+            contentType
+          }
+        }
+        meshGridBottom {
+          file {
+            contentType
+            url
+          }
+        }
+      }
+    }
   }
 `
 
@@ -21,7 +74,12 @@ const ReelPage = () => (
   <StaticQuery
     query={reelQuery}
     render={data => {
+      const { nodes } = data.allContentfulOurWork
       const { vimeoId } = data.contentfulReel
+      const {
+        meshGridBottom,
+        meshGridBottomMobile,
+      } = data.allContentfulBlackPageMeshGrids.nodes[0]
       return (
         <ThemeProvider theme={theme}>
           <Layout className="reel-page">
@@ -36,6 +94,12 @@ const ReelPage = () => (
                 allowFullScreen
               ></iframe>
             </ReelWrapper>
+            <Footer
+              textColor="light"
+              content={nodes[0].footer}
+              mobileBgm={meshGridBottomMobile}
+              bgm={meshGridBottom}
+            />
           </Layout>
         </ThemeProvider>
       )
